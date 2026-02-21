7dc9aa4675c3dd74170c2d77003b2aac4475aeae
knaaptime <knaaptime@anteater.local>
2026-02-21 15:21:04 -0800

    test single geom

--- tests/test_path_geom.py
+++ tests/test_path_geom.py
@@ -4,10 +4,16 @@ net = pdna.Network.from_hdf5("tests/uci_net.h5")
 o_node = 3710893961
 d_node = 6556054235
 
-def test_shortest_path_geom():
+def test_shortest_path_multi_geom():
     geom = net.shortest_path_geoms([o_node], [d_node])
 
     assert (
         round(geom.union_all().length, 3)
         == 0.017
     )
+
+
+def test_shortest_path_single_geom():
+    geom = net.shortest_path_geometry(o_node, d_node)
+
+    assert round(geom.union_all().length, 3) == 0.017
